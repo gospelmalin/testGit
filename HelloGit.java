a7c8a2031e40f8343bfd2722a120627767d40e4a
Malin Albinsson <mea733@telia.com>
2018-01-28 16:40:49 +0100

    3rd try to push to branch

--- HelloGit.java
+++ HelloGit.java
@@ -1,7 +1,7 @@
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git! This is developer branch 2nd try");
+		System.out.println("Hello Git! This is developer branch 3rd try");
 
 	}
 
